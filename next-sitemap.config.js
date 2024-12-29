4bd95192c43a006834ad7f2fe981c4fc93be1eca
Abhijith <abhijithj331@gmail.com>
2024-12-29 10:08:44 +0530

    meta tags added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
-// next-sitemap.config.js
+// next-sitemap.js
+
 module.exports = {
-    siteUrl: 'https://storiesbymidhunshyam.netlify.app', // Your Netlify URL
-    generateRobotsTxt: true, // Optional: Generate robots.txt file
+    siteUrl: 'https://storiesbymidhunshyam.netlify.app',  // Replace with your live URL
+    generateRobotsTxt: true,  // Optional: Automatically generates a robots.txt file
+    sitemapSize: 7000,  // Optional: Defines the max number of URLs per sitemap file
+    outDir: './out',  // This will output the sitemap to the `out` directory
   };
   
